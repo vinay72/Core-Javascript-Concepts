a0d0f4324ff0d0d6b2f88314a6a796cbf939abb4
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2025-10-04 02:11:40 +0530

    Update currying_using_closures.js

--- Function Currying/currying_using_closures.js
+++ Function Currying/currying_using_closures.js
@@ -1,5 +1,10 @@
 // function currying using closures
 
+Currying in JavaScript is a functional programming technique that
+transforms a function taking multiple arguments into a sequence of functions,each taking a single argument.
+This process creates a chain of functions that accumulate arguments until all necessary parameters are provided,
+at which point the final function executes and returns a result. 
+
 let multiply = function (x){ // presetting the x
     return function(y){ // presetting the y
         console.log(x * y);
