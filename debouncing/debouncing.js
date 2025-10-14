b729eeda4269e91436e93e3ba942a92f65012575
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2025-10-15 00:03:20 +0530

    Update debouncing.js

--- debouncing/debouncing.js
+++ debouncing/debouncing.js
@@ -5,28 +5,36 @@
 // You almost certainly have encountered debouncing in your daily lives before — when entering an elevator. 
 // Only after X duration of not pressing the "Door open" button (the debounced function not being called) will the elevator door actually close (the callback function is executed).
 
-// FLIPKART UI INTERVIEW QUESTION
 let counter = 0
+
 const getData = () => {
-    // calls an api and gets data
-    console.log("Fetching data...", counter++)
+    // calls an API and gets the data
+    console.log("Fetching Data..", counter++);
+    
 }
+// what we want to achieve is when we pause then only event should trigger or api 
+// call would be made
 
-const doSomeMagic = (fn, d) => {
-    // function and delay as two arguments
-    // d is delay between two keypress events
-    let timer; // initialised timer
-    return function () {
-
+// debounce function
+const debounce = function(fn,delay){
+    let timer;
+    // fn is getData in this case, debounce function will not let this getData function to make unnecessary network calls.
+    // delay is time between two keystroke press events
+    return function() {
         let context = this;
-        args = arguments;
-        // these are required to keep a check that the environment of the lexical scope
-        // where this function is running is correct and getData would be called with the same arguments if it had.
-        clearTimeout(timer);
+         args = arguments;
+         clearTimeout(timer);
+// this timer has setTimeout which when expires this delay of 300 milliseconds will call this getData method but if keystroke happens between this time, we need to clear this timer         
         timer = setTimeout(() => {
             getData.apply(context, arguments)
-        }, d)
+        }, delay)
+        // if again a keystroke happens while pausing, then we should stop calling this method
     }
 }
 
-const betterfunction = doSomeMagic(getData, 300)
+const betterFunction = debounce(getData, 300)
+// if the time difference between two key presses is greater than 300 milliseconds then only call getData method,
+// then only fetch the results from the api
+// this should only call when user paused typing , not again and again
+
+// the purpose is to reduce the no of network calls made and by using debounce function we can reduce the no on keypress events and we can optimize the performance of our web page.
